f153d378066b540ec979a214951de251e08a71fe
thevillas <villalbas991@gmail.com>
2024-01-03 14:53:44 -0500

    backend

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -22,16 +22,68 @@ router.post('/ingreso', async (req, res) => {
     const userFound = await user.findOne({email})
 
     if (!userFound) return res.status(401).send("el correo o la contraseña no existen");
-    if (userFound.password !== password) return res.status(401).send("el correo o la contraseña no esisten");
+    if (userFound.password !== password) return res.status(401).send("el correo o la contraseña no existen");
 
     const token = jwt.sign({_id: userFound.id}, 'secretkey'); 
     return res.status(200).json({token});
 
 });
 
+router.get('/tasks', (req, res) =>{
+    res.json([
+        {
+            _id: 1,
+            name: "task",
+            description: "lorem Ipsum"
+        },{
+            _id: 1,
+            name: "task",
+            description: "lorem Ipsum"
+        }
+    ]);
+});
+
+router.get('/tasks-private', verifyToken, (req, res) =>{
+    res.json([
+        {
+            _id: 1,
+            name: "task",
+            description: "lorem Ipsum"
+        },{
+            _id: 1,
+            name: "task",
+            description: "lorem Ipsum"
+        }
+    ]);
+});
+
+router.get('/profile', verifyToken, (req, res)=>{
+    res.send(req.userId); 
+})
+
+
+
+async function verifyToken(req, res, next) {
+	try {
+		if (!req.headers.authorization) {
+			return res.status(401).send('busqueda no valida');
+		}
+		let token = req.headers.authorization.split(' ')[1];
+		if (token === 'null') {
+			return res.status(401).send('busqueda no valida');
+		}
+
+		const payload = await jwt.verify(token, 'secretkey');
+		if (!payload) {
+			return res.status(401).send('busqueda no valida');
+		}
+		req.userId = payload._id;
+		next();
+	} catch(e) {
+		return res.status(401).send('busqueda no valida');
+	}
+}
 
 
 
 exports.router = router;
-
-
